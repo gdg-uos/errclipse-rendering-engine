856726289761ffad2dba3441a0024bf86ef0250b
stephan lee <toori67@gmail.com>
2012-10-14 02:00:45 +0900

    test

--- errclipse/errclipseRedenringEngine/src/main/java/com/errclipse/runner.java
+++ errclipse/errclipseRedenringEngine/src/main/java/com/errclipse/runner.java
@@ -5,7 +5,7 @@ import com.errclipse.orm.connector.ConnectToORM;
 public class runner {
     public static void main(String args[]){
     	System.out.println("start");
-    	ConnectToORM.getLangID("c");
+    	ConnectToORM.getLangID("java16");
 
     }
 }
