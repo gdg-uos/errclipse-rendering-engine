d5429a10aca946181a5d216cc267707a2dd52960
stephan <stephan@stephan-ThinkPad-T400.(none)>
2012-10-15 01:27:46 +0900

    fully done

---
+++ errclipse/errclipseRedenringEngine/src/main/java/com/errclipse/orm/bin/SolutionResultBin.java
@@ -0,0 +1,19 @@
+package com.errclipse.orm.bin;
+
+public class SolutionResultBin {
+	int solution_id;
+	String solution_desc;
+	public int getSolution_id() {
+		return solution_id;
+	}
+	public void setSolution_id(int solution_id) {
+		this.solution_id = solution_id;
+	}
+	public String getSolution_desc() {
+		return solution_desc;
+	}
+	public void setSolution_desc(String solution_desc) {
+		this.solution_desc = solution_desc;
+	}
+
+}
