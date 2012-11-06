0439343d1a2b4fb82bf02638c52010c68674d18e
stephan lee <toori67@gmail.com>
2012-11-06 17:52:56 +0900

    fix sql.xml and recommendation function added

---
+++ errclipse/DistDefender/src/main/java/com/errclipse/child/ChildPropGrabber.java
@@ -0,0 +1,8 @@
+package com.errclipse.child;
+
+/*
+ * get child_property from child's web-server
+ */
+public class ChildPropGrabber {
+
+}
---
+++ errclipse/DistDefender/src/main/java/com/errclipse/child/ChildPropRepo.java
@@ -0,0 +1,9 @@
+package com.errclipse.child;
+
+/*
+ * manage child's information, write to DB, load from DB , etc
+ */
+
+public class ChildPropRepo {
+	
+}
